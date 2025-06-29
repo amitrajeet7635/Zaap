ed689567e45cd92d4020a889a4b6b44a81b1e9a2
Ambarish Manna <amanna1041@gmail.com>
2025-06-29 19:06:50 +0530

    Code cleanup

--- Zaap-Android/app/src/main/java/com/zaap/app/presentation/features/ScanAndPay.kt
+++ Zaap-Android/app/src/main/java/com/zaap/app/presentation/features/ScanAndPay.kt
@@ -3,15 +3,9 @@ package com.zaap.app.presentation.features
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,25 +15,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.mlkit.vision.barcode.common.Barcode
-import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.zaap.app.presentation.modules.ScanAndPayScanner
 import com.zaap.app.presentation.viewmodel.ScanDataViewModel
-import androidx.hilt.navigation.compose.hiltViewModel
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun ScanAndPay(modifier: Modifier = Modifier, navHostController: NavHostController ,scanDataViewModel: ScanDataViewModel = hiltViewModel()) {
+fun ScanAndPay(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController,
+    scanDataViewModel: ScanDataViewModel = hiltViewModel()
+) {
 
     var scannedResult by remember { mutableStateOf<String?>(null) }
     val permissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
@@ -79,107 +72,8 @@ fun ScanAndPay(modifier: Modifier = Modifier, navHostController: NavHostControll
                     scanDataViewModel.setScannedData(result)
                     navHostController.navigate("ScanPayCheckout")
                 }
-
-//                scannedResult?.let { result ->
-//                    Card(
-//                        modifier = Modifier
-//                            .fillMaxWidth(0.8f) // 80% width of its parent
-//                            .padding(16.dp)
-//                            .align(Alignment.BottomCenter), // Position at the bottom center
-//                        shape = RoundedCornerShape(12.dp), // Rounded corners
-//                        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp), // Shadow
-//                        colors = CardDefaults.cardColors(containerColor = Color.White) // White background for the card
-//                    ) {
-//                        Column(
-//                            modifier = Modifier
-//                                .padding(20.dp)
-//                                .fillMaxWidth(),
-//                            horizontalAlignment = Alignment.CenterHorizontally
-//                        ) {
-//                            Text(
-//                                text = "Scanned QR Code:",
-//                                fontSize = 18.sp,
-//                                fontWeight = FontWeight.Bold,
-//                                color = Color.DarkGray
-//                            )
-//                            Spacer(modifier = Modifier.height(8.dp))
-//                            Text(
-//                                text = result,
-//                                fontSize = 16.sp,
-//                                color = Color.Black,
-//                                textAlign = TextAlign.Center
-//                            )
-//                            Spacer(modifier = Modifier.height(8.dp))
-//                            Text(
-//                                text = "Processing payment...", // Placeholder for actual payment logic
-//                                fontSize = 14.sp, color = Color.Gray
-//                            )
-//                        }
-//                    }
-//                }
             }
         }
     }
 }
 
-
-//@OptIn(ExperimentalPermissionsApi::class)
-//@Composable
-//fun ScanAndPay(onCodeScanned: (String) -> Unit) {
-//    val permissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
-//    val context = LocalContext.current
-//    val lifecycleOwner = LocalLifecycleOwner.current
-//
-//    val options = GmsBarcodeScannerOptions.Builder().setBarcodeFormats(
-//            Barcode.FORMAT_QR_CODE,
-//        ).enableAutoZoom().build()
-//
-//
-//
-//    if (permissionState.status.isGranted) {
-//
-//    } else {
-//        Box(
-//            Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-//        ) {
-//            Text("Camera permission required", color = Color.Red)
-//        }
-//    }
-//}
-
-
-//@Composable
-//fun CameraPreviewView(modifier: Modifier = Modifier) {
-//    val context = LocalContext.current
-//    val lifecycleOwner = LocalLifecycleOwner.current
-//
-//    AndroidView(
-//        modifier = modifier,
-//        factory = { ctx ->
-//            val previewView = PreviewView(ctx)
-//
-//            val cameraProviderFuture = ProcessCameraProvider.getInstance(ctx)
-//            cameraProviderFuture.addListener({
-//                val cameraProvider = cameraProviderFuture.get()
-//
-//                val preview = Preview.Builder().build().also {
-//                    it.setSurfaceProvider(previewView.surfaceProvider)
-//                }
-//
-//                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-//
-//                try {
-//                    cameraProvider.unbindAll()
-//                    cameraProvider.bindToLifecycle(
-//                        lifecycleOwner, cameraSelector, preview
-//                    )
-//                } catch (e: Exception) {
-//                    e.printStackTrace()
-//                }
-//
-//            }, ContextCompat.getMainExecutor(ctx))
-//
-//            previewView
-//        }
-//    )
-//}
