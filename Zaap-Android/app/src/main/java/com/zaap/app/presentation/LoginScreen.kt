1a3192154d193901a6a3493c7ef0d5868092f938
Ambarish Manna <amanna1041@gmail.com>
2025-07-04 03:55:11 +0530

    Login Page UI fix

--- Zaap-Android/app/src/main/java/com/zaap/app/presentation/LoginScreen.kt
+++ Zaap-Android/app/src/main/java/com/zaap/app/presentation/LoginScreen.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.ContextWrapper
 import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -34,7 +35,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -174,7 +177,7 @@ fun LoginScreen(
 //            }
 //        }
 //    }
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier.fillMaxSize().background(Color(0xFF9cacff)), contentAlignment = Alignment.Center) {
         Column(
             modifier = Modifier
                 .padding(24.dp)
@@ -321,6 +324,5 @@ fun OrDivider(
         )
     }
 }
-
 
 
